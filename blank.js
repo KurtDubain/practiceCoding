5d6b3ef4952904eba5228ea83bdbf6b54722eee1
KurtDubain <131502852+KurtDubain@users.noreply.github.com>
2023-10-13 20:29:59 +0800

    优化

--- blank.js
+++ blank.js
@@ -39,42 +39,16 @@
 // })
 // console.log('1',promise1)
 // console.log('2',promise2)
-class NodeList{
-  constructor(val,next=null){
-    this.val = val
-    this.next = null
-  }
-}
-function isBackNode(node){
-  if(!node&&!node.next){
-    return true
-  }
-  let slow = head
-  let quick = head
-  while(slow.next&&quick.next.next){
-    slow = slow.next
-    quick = quick.next.next
-  }
-  let midHead = reverse(slow)
-  let p1 = head
-  let p2 = midHead
-  while(p1.next&&p2.next){
-    if(p1.val!==p2.val){
-      return false
-    }
-  }
-  return true
-}
-
-function reverse(node){
-  let cur = node
-  let pre = null
-  while(cur){
-    let next = cur.next
-    cur.next = pre
-    pre = cur
-    cur = next
-    
-  }
-  return pre
-}
+(async()=>{
+  console.log(1)
+  setTimeout(()=>{
+    console.log(2)
+  },0)
+  await new Promise((resolve,reject)=>{
+    console.log(3)
+  })
+  .then(()=>{
+    console.log(4)
+  })
+  console.log(5)
+})()
