e2385e7d1ef987a8108d168f76544ddb23155055
KurtDubain <131502852+KurtDubain@users.noreply.github.com>
2023-10-17 21:33:34 +0800

    1

--- blank.js
+++ blank.js
@@ -1,54 +1,71 @@
-// function getMaxDisjointSubarrayProduct(nums) {
-//     if (nums.length === 0) {
-//       return 0;
-//     }
+// function findSubarrayWithSumK(nums, k) {
+//     const map = new Map(); // 用于存储前缀和及其出现次数
+//     map.set(0, [-1]); // 初始化前缀和为0的索引数组为[-1]
+//     let sum = 0; // 当前的前缀和
+//     const result = []; // 存储和为 k 的子数组
   
-//     let maxProduct = nums[0];
-//     let positiveMax = nums[0];
-//     let negativeMax = nums[0];
-  
-//     for (let i = 1; i < nums.length; i++) {
-//       if (nums[i] > 0) {
-//         positiveMax = Math.max(nums[i], positiveMax * nums[i]);
-//         negativeMax = negativeMax * nums[i];
-//       } else if (nums[i] < 0) {
-//         const temp = positiveMax;
-//         positiveMax = Math.max(nums[i], negativeMax * nums[i]);
-//         negativeMax = Math.min(nums[i], temp * nums[i]);
-//       } else {
-//         positiveMax = 0;
-//         negativeMax = 0;
+//     for (let i = 0; i < nums.length; i++) {
+//       sum += nums[i];
+//       if (map.has(sum - k)) {
+//         const indexes = map.get(sum - k); // 获取前缀和为 sum - k 的索引数组
+//         for (const index of indexes) {
+//           result.push(nums.slice(index + 1, i + 1)); // 将子数组加入结果数组
+//         }
 //       }
-  
-//       maxProduct = Math.max(maxProduct, positiveMax);
+//       if (!map.has(sum)) {
+//         map.set(sum, []);
+//       }
+//       map.get(sum).push(i); // 将当前前缀和的索引加入索引数组
 //     }
-  
-//     return maxProduct;
+//     return result;
 //   }
   
-//   // 示例用法
-//   const nums = [2, -3, 1, 0, -2, 4, -5, -6, 3];
-//   const maxProduct = getMaxDisjointSubarrayProduct(nums);
-//   console.log(maxProduct);
-// const promise1 = new Promise((resolve,reject)=>{
-//   console.log('promise1')
-//   resolve('resolve1')
-// })
-// const promise2 = promise1.then(res=>{
-//   console.log(res)
-// })
-// console.log('1',promise1)
-// console.log('2',promise2)
-(async()=>{
-  console.log(1)
-  setTimeout(()=>{
-    console.log(2)
-  },0)
-  await new Promise((resolve,reject)=>{
-    console.log(3)
-  })
-  .then(()=>{
-    console.log(4)
-  })
-  console.log(5)
-})()
+// function subarraySum(nums, k) {
+//     let count = 0; // 计数器，记录和为 k 的子数组个数
+//     let sum = 0; // 前缀和
+//     const prefixSumMap = new Map(); // 前缀和及其出现次数的映射表
+//     prefixSumMap.set(0, [-1]); // 初始化前缀和为 0 的索引为 -1
+  
+//     const result = []; // 用于存储和为 k 的子数组的起始和终止索引
+  
+//     for (let i = 0; i < nums.length; i++) {
+//       sum += nums[i];
+//       if (prefixSumMap.has(sum - k)) {
+//         const indices = prefixSumMap.get(sum - k);
+//         count += indices.length;
+//         result.push(...indices.map((index) => [index + 1, i])); // 将起始和终止索引存入 result
+//       }
+//       if (prefixSumMap.has(sum)) {
+//         prefixSumMap.get(sum).push(i);
+//       } else {
+//         prefixSumMap.set(sum, [i]);
+//       }
+//     }
+  
+//     return { count, result };
+//   }
+
+function findSubarray(nums, k) {
+    const result = [];
+  
+    function backtrack(start, currSum, currArr) {
+      if (currSum === k) {
+        result.push([...currArr]);
+      }
+  
+      for (let i = start; i < nums.length; i++) {
+        currArr.push(nums[i]);
+        backtrack(i + 1, currSum + nums[i], currArr);
+        currArr.pop();
+      }
+    }
+  
+    backtrack(0, 0, []);
+  
+    return result;
+  }
+  // 使用示例
+  const nums = [3, 1, 7, 2, -3, 1, 4];
+  const k = 7;
+  const subarrays = findSubarray(nums, k);
+  console.log(subarrays);
