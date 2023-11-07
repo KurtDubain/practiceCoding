d9de8cd4a97d8546a71f0914e55ff768b4cbb265
KurtDubain <131502852+KurtDubain@users.noreply.github.com>
2023-11-07 23:02:36 +0800

    更新

---
+++ blank.js
@@ -0,0 +1,17 @@
+console.log(JSON.parse('[1,2,[{"1":2,"a":23},2],3]'))
+// const arr1 = [1,2,[{1:2,a:23},2],3]
+// console.log(arr1)
+// const obj1 = {
+//     '1':231,
+//     1:3333333
+// }
+const arr2 = [
+    {
+        '1':'dhaos'
+    },
+    {
+        a:2132
+    }
+
+]
+// console.log(JSON.stringify(arr2))
