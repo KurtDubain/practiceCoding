923999b3f21ebd5ebe5604ab64a99ceb31770883
KurtDubain <131502852+KurtDubain@users.noreply.github.com>
2023-11-07 22:16:43 +0800

    更新KMP

--- other.js
+++ other.js
@@ -483,3 +483,96 @@ function deepEqual(obj1, obj2) {
   // 所有属性都相等
   return true;
 }
+// kmp算法
+function buildPatternTable(pattern) {
+  const table = [0];
+  let prefixIndex = 0;
+  let suffixIndex = 1;
+
+  while (suffixIndex < pattern.length) {
+    if (pattern[prefixIndex] === pattern[suffixIndex]) {
+      table[suffixIndex] = prefixIndex + 1;
+      prefixIndex++;
+      suffixIndex++;
+    } else if (prefixIndex === 0) {
+      table[suffixIndex] = 0;
+      suffixIndex++;
+    } else {
+      prefixIndex = table[prefixIndex - 1];
+    }
+  }
+
+  return table;
+}
+
+function kmpSearch(text, pattern) {
+  const patternTable = buildPatternTable(pattern);
+  let textIndex = 0;
+  let patternIndex = 0;
+  const matchedIndices = [];
+
+  while (textIndex < text.length) {
+    if (text[textIndex] === pattern[patternIndex]) {
+      if (patternIndex === pattern.length - 1) {
+        matchedIndices.push(textIndex - pattern.length + 1);
+        patternIndex = 0;
+        textIndex++;
+      } else {
+        patternIndex++;
+textIndex++;
+      }
+    } else if (patternIndex > 0) {
+      patternIndex = patternTable[patternIndex - 1];
+    } else {
+      textIndex++;
+    }
+  }
+
+  return matchedIndices;
+}
+// 改进kmp算法
+function buildPatternTable(pattern) {
+  const table = [0];
+  let prefixIndex = 0;
+  let suffixIndex = 1;
+
+  while (suffixIndex < pattern.length) {
+    if (pattern[prefixIndex] === pattern[suffixIndex]) {
+      table[suffixIndex] = prefixIndex + 1;
+      prefixIndex++;
+      suffixIndex++;
+    } else if (prefixIndex === 0) {
+      table[suffixIndex] = 0;
+      suffixIndex++;
+    } else {
+      prefixIndex = table[prefixIndex - 1];
+    }
+  }
+
+  return table;
+}
+
+function kmpSearch(text, pattern) {
+  const patternTable = buildPatternTable(pattern);
+  let textIndex = 0;
+  let patternIndex = 0;
+  const matchedIndices = [];
+
+  while (textIndex < text.length) {
+    if (text[textIndex] === pattern[patternIndex]) {
+      if (patternIndex === pattern.length - 1) {
+        matchedIndices.push(textIndex - pattern.length + 1);
+        patternIndex = patternTable[patternIndex];
+      } else {
+        textIndex++;
+        patternIndex++;
+      }
+    } else if (patternIndex > 0) {
+      patternIndex = patternTable[patternIndex - 1];
+    } else {
+      textIndex++;
+    }
+  }
+
+  return matchedIndices;
+}
