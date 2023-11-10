a1b2984cf2df4f3ad08c3a2c40f93720bf0241e7
KurtDubain <131502852+KurtDubain@users.noreply.github.com>
2023-11-10 22:32:42 +0800

    定义了30个算法题目

--- blank2.js
+++ blank2.js
@@ -475,3 +475,33 @@ function changeK(str){
     }
     return decArr?`${result.join('')}.${decArr}`:result.join('')
 }
+// 26、括号闭合问题
+function isClosed(str){
+    if(!str){
+        return true
+    }
+
+    const queue = []
+    const left = ['(','[','{']
+    const right = [')',']','}']
+    for(let i = 0; i<str.length; i++){
+        const firstStr = str[i]
+        if(left.includes(firstStr)){
+            queue.push(firstStr)
+
+        }else if(right.includes(firstStr)){
+            let ShouldLast = queue.pop()
+            if( ShouldLast !== left[right.indexOf(firstStr)]){
+                return false
+            }
+        }
+    }
+    return true
+}
+// 27、EventBus
+
+// 28、Observer
+
+// 29、Promise的retry
+
+// 30、Promise的递归调用
