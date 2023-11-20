a3b4cb6c2cb4fbfeba84ac9eb5ebc4bf6cc15fd3
KurtDubain <131502852+KurtDubain@users.noreply.github.com>
2023-11-20 22:20:23 +0800

    1

--- tree.js
+++ tree.js
@@ -244,3 +244,17 @@ function reverseTree(root){
 
     return root
 }
+// 数组转树
+function ArrayToTree(arrayData){
+    let result = []
+    for(let i = 0;i<arrayData.length;i++){
+        let item = arrayData[i]
+        let children = item.children
+        result.push(item)
+        if(children&&children.length){
+            // ArrayToTree(children)
+            result = result.concat(arrayData(children))
+        }
+    }
+    return result
+}
