8b65346b1c14167f776b085a3319386f290493eb
KurtDubain <131502852+KurtDubain@users.noreply.github.com>
2023-11-22 23:42:46 +0800

    新增问题

--- blank.js
+++ blank.js
@@ -1,20 +1,13 @@
-function permute(nums) {
-    const result = [];
-    
-    function backtrack(start) {
-      if (start === nums.length) {
-        result.push([...nums]);
-        return;
-      }
-      
-      for (let i = start; i < nums.length; i++) {
-        [nums[start], nums[i]] = [nums[i], nums[start]];
-        backtrack(start + 1); 
-        [nums[start], nums[i]] = [nums[i], nums[start]];
-      }
-    }
-    
-    backtrack(0);
-    return result;
+var a = 0
+var b = 0
+var c = 0
+function fn(a){
+  console.log(a++,c)
+  function fn2(b){
+    console.log(a,b,c)
   }
-  console.log(permute([1,2,3]))
+  var c = 4
+  fn = fn2
+}
+fn(1)
+fn(2)
