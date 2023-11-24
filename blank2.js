caa6a8cef6fc6b1762ec6178a713c3d31a554558
KurtDubain <131502852+KurtDubain@users.noreply.github.com>
2023-11-24 10:30:35 +0800

    新增了md模版

--- blank2.js
+++ blank2.js
@@ -452,18 +452,23 @@ function findLongStr(str){
     let start = 0
     let end = 0
     let maxSum = 0
+    let maxStart = 0
     const remSet = new Set()
     while(start<str.length&&end<str.length){
         if(!remSet.has(str[end])){
             remSet.add(str[end],end)
-            maxSum = Math.max(maxSum,end-start+1)
+            // maxSum = Math.max(maxSum,end-start+1)
+            if(end-start+1>maxSum){
+                maxSum = end-start+1
+                maxStart = start
+            }
             end++
         }else{
             remSet.delete(str[start])
             start++
         }
     }
-    return maxSum
+    return str.substr(maxStart,maxSum)
 }
 // 25、千分位转换
 function changeK(str){
