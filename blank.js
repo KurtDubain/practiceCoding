4a5e8daa98dc2e5fcbcbd3bbff12d4b29db2bb6b
Kurt.Dubain <kurt.du.cobain@gmail.com>
2024-01-19 13:38:12 +0800

    测试

--- blank.js
+++ blank.js
@@ -1,6 +1,19 @@
 // 1、大数相加
 function bigNumAdd(str1,str2){
-
+    let i = str1.length-1
+    let j = str2.length-1
+    let result = []
+    let add = 0
+    while(i>=0||j>=0||add>0){
+        let curStr1 = i>=0?parseInt(str1[i]):0
+        let curStr2 = j>=0?parseInt(str2[j]):0
+        const sum = Math.floor(curStr1+curStr2+add)%10
+        result.unshift(sum)
+        add = Math.floor(curStr1+curStr2)/10
+        i--
+        j--
+    }
+    return result.join('')
 }
 
 // 2、instanceof手写
@@ -40,10 +53,31 @@ function debounce(func,delay){
 }
 // 4、对象的dfs（递归、非递归）和bfs和层级遍历多叉树
 function dfsObj1(obj){
-
+    if(!obj){
+        return null
+    }
+    console.log(obj)
+    for(let key in obj){
+        if(typeof(obj[key])==='object'&&obj[key]!==null){
+            dfsObj1(obj[key])
+        }
+    }
+    
 }
 function dfsObj2(obj){
-
+    if(!obj){
+        return null
+    }
+    const stack = [obj]
+    while(stack>0){
+        const curObj = stack.pop()
+        console.log(curObj)
+        for(let key in curObj){
+            if(typeof(curObj[key])==="object"&&curObj[key]!==null){
+                stack.push(curObj[key])
+            }
+        }
+    }
 }
 function bfsObj(obj){
 
@@ -104,15 +138,50 @@ function parseArr(str){
 
 // 7、手写固定和
 function findSum(arr,target){
-
+    const map = new Map()
+    const result = []
+    for(let i = 0;i<arr.length;i++){
+        if(map.has(target-arr[i])){
+            result.push([arr[i],target-arr[i]])
+        }
+        map.set(arr[i],arr[i])
+    }
+    return result
 }
 // 8、函数柯里化func(1)(2)(3)
 function curry(func){
-
+    return function curried(...args){
+        if(args.length>=func.length){
+            return func.apply(this,args)
+        }else{
+            return function(...newArgs){
+                return curried.apply(this,args.concat(newArgs))
+            }
+        }
+    }
 }
 // 9、手写Promise以及then以及All
 function promiseAll(arr){
-
+    return new Promise((resolve,reject)=>{
+        let count = 0
+        if(arr.length<1){
+            resolve([])
+        }
+        const results = []
+        for(let i = 0;i<arr.length;i++){
+            arr[i]
+            .then((result)=>{
+                count++
+                results[i] = result
+                if(count===arr.length){
+                    resolve(results)
+                }
+            })
+            .catch(error=>{
+                reject(error)
+            })
+        }
+    })
 }
 
 // 10、长字符串在长文本中的查找
@@ -121,7 +190,35 @@ function findLongStr(strDad,strSon){
 }
 // 11、实现LRU
 class LRUCache{
-
+    constructor(capcity){
+        this.capcity = capcity
+        this.cache = new Map()
+    }
+    get(key){
+        if(this.cache.has(key)){
+            const curKeyVal = this.cache.get(key)
+            this.cache.delete(key)
+            this.cache.set(key,curKeyVal)
+            return curKeyVal
+        }
+        return -1
+    }
+    add(key,value){
+        if(this.cache.has(key)){
+            this.cache.delete(key)
+        }else{
+            if(this.cache.size>this.capcity){
+                const lastKey = this.cache.keys().next().value
+                this.cache.delete(lastKey)
+            }
+        }
+        this.cache.set(key,value)
+    }
+    delete(key){
+        if(this.cache.has(key)){
+            this.cache.delete(key)
+        }
+    }
 }
 // 12、call、bind、apply方法手写
 function myCall(context,arg){
@@ -133,15 +230,33 @@ function myCall(context,arg){
     return result
 }
 function myApply(context,...args){
-
+    context = context||window
+    const fn = Symbol('fn')
+    context[fn]=this
+    const result = context[fn](...args)
+    delete context[fn]
+    return result
 }
 // 13、深拷贝（解决循环应用）
 function Deepclone(obj,visited = new Map()){
-
+    if(typeof obj !=='object'||obj===null){
+        return obj
+    }
+    if(visited.has(obj)){
+        return visited.get(obj)
+    }
+    const cloneObj = Array.isArray(obj)?[]:{}
+    visited.set(obj,cloneObj)
+    for(let key in obj){
+        if(obj.hasOwnProperty(key)){
+            cloneObj[key] = Deepclone(obj[key],visited)
+        }
+    }
+    return cloneObj
 }
 // 14、翻转二叉树
 function reverseTree(root){
-
+    
 }
 // 15、零钱兑换
 function findMoney(account,coins){
