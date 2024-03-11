0956b1f0f3088891d011bfe6973a591430bdc7de
Kurt.Dubain <kurt.du.cobain@gmail.com>
2024-03-11 20:02:49 +0800

    Update haha.js

--- haha.js
+++ haha.js
@@ -1,5 +1,36 @@
-const set = new Set();
-set.add("jj", "sa");
-console.log(set.keys());
-console.log(set.values());
-console.log(set.entries());
+async function a() {
+  console.log(1);
+  await b();
+  console.log(2);
+}
+async function b() {
+  new Promise((resolve, reject) => {
+    console.log(3);
+    resolve(4);
+    console.log(5);
+  })
+    .then((val) => {
+      console.log(val);
+      throw new Error(6);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+}
+console.log(7);
+a();
+setTimeout(() => {
+  console.log(8);
+}, 0);
+const p = new Promise((resolve, reject) => {
+  console.log(9);
+  resolve(10);
+}).then((val) => {
+  console.log(val);
+});
+p.then((val) => {
+  console.log("p:then", val);
+});
+console.log("script end");
+
+// 7 1 3 9 script end
