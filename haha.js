acaed718ef2928f8464bb3b8fead5d3a3e765056
Kurt.Dubain <kurt.du.cobain@gmail.com>
2024-03-12 21:18:59 +0800

    Update haha.js

--- haha.js
+++ haha.js
@@ -1,36 +1,28 @@
-async function a() {
-  console.log(1);
-  await b();
-  console.log(2);
+// new Promise((resolve) => {
+//   resolve(1);
+//   resolve(2);
+//   console.log("test");
+// }).then((res) => {
+//   console.log(res);
+// });
+async function async1() {
+  console.log("async1 start");
+  await async2();
+  console.log("async1 end");
 }
-async function b() {
-  new Promise((resolve, reject) => {
-    console.log(3);
-    resolve(4);
-    console.log(5);
-  })
-    .then((val) => {
-      console.log(val);
-      throw new Error(6);
-    })
-    .catch((err) => {
-      console.log(err);
-    });
+async function async2() {
+  console.log("async2");
 }
-console.log(7);
-a();
+console.log("script start");
 setTimeout(() => {
-  console.log(8);
+  console.log("setTimeout");
 }, 0);
-const p = new Promise((resolve, reject) => {
-  console.log(9);
-  resolve(10);
-}).then((val) => {
-  console.log(val);
-});
-p.then((val) => {
-  console.log("p:then", val);
+async1();
+new Promise((resolve) => {
+  console.log("promise1");
+  resolve();
+}).then(() => {
+  console.log("promise2");
 });
 console.log("script end");
-
-// 7 1 3 9 script end
+// script start ->async1 start->async2->promise1->script end->async1 end->promise2->setTimeout
