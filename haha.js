a31d44877c3a4794132c0410fc487b20751f6e4b
Kurt.Dubain <kurt.du.cobain@gmail.com>
2024-03-13 22:12:25 +0800

    Update haha.js

--- haha.js
+++ haha.js
@@ -1,28 +1,45 @@
-// new Promise((resolve) => {
-//   resolve(1);
-//   resolve(2);
-//   console.log("test");
-// }).then((res) => {
-//   console.log(res);
-// });
-async function async1() {
-  console.log("async1 start");
-  await async2();
-  console.log("async1 end");
+// function quickSort(arr, left = 0, right = arr.length - 1) {
+//   if (left < right) {
+//     let p = getP(arr, left, right);
+//     quickSort(arr, left, p - 1);
+//     quickSort(arr, p + 1, right);
+//   }
+//   return arr;
+// }
+// function getP(arr, left, right) {
+//   let p = arr[right];
+//   let i = left;
+//   for (let j = left; j < right; j++) {
+//     if (arr[j] < p) {
+//       [arr[i], arr[j]] = [arr[j], arr[i]];
+//       i++;
+//     }
+//   }
+//   [arr[i], arr[right]] = [arr[right], arr[i]];
+//   return i;
+// }
+// let arr = [3, 6, 8, 10, 1, 2, 1];
+// console.log(quickSort(arr));
+function threeSum(arr) {
+  const result = [];
+  arr.sort((a, b) => a - b);
+  for (let i = 0; i < arr.length; i++) {
+    let left = i + 1;
+    let right = arr.length - 1;
+    while (left < right) {
+      const sum = arr[i] + arr[left] + arr[right];
+      if (sum === 0) {
+        result.push([arr[i], arr[left], arr[right]]);
+        left++;
+        right--;
+      } else if (sum < 0) {
+        left++;
+      } else if (sum > 0) {
+        right--;
+      }
+    }
+  }
+  return result;
 }
-async function async2() {
-  console.log("async2");
-}
-console.log("script start");
-setTimeout(() => {
-  console.log("setTimeout");
-}, 0);
-async1();
-new Promise((resolve) => {
-  console.log("promise1");
-  resolve();
-}).then(() => {
-  console.log("promise2");
-});
-console.log("script end");
-// script start ->async1 start->async2->promise1->script end->async1 end->promise2->setTimeout
+const arr = [-1, 0, 1, 2, -1, -4];
+console.log(threeSum(arr));
