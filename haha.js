1420857963e3e840d08cb09c8ec35230a247a7b8
Kurt.Dubain <kurt.du.cobain@gmail.com>
2024-03-14 17:15:10 +0800

    Update haha.js

--- haha.js
+++ haha.js
@@ -1,45 +1,22 @@
-// function quickSort(arr, left = 0, right = arr.length - 1) {
-//   if (left < right) {
-//     let p = getP(arr, left, right);
-//     quickSort(arr, left, p - 1);
-//     quickSort(arr, p + 1, right);
-//   }
-//   return arr;
-// }
-// function getP(arr, left, right) {
-//   let p = arr[right];
-//   let i = left;
-//   for (let j = left; j < right; j++) {
-//     if (arr[j] < p) {
-//       [arr[i], arr[j]] = [arr[j], arr[i]];
-//       i++;
-//     }
-//   }
-//   [arr[i], arr[right]] = [arr[right], arr[i]];
-//   return i;
-// }
-// let arr = [3, 6, 8, 10, 1, 2, 1];
-// console.log(quickSort(arr));
-function threeSum(arr) {
-  const result = [];
-  arr.sort((a, b) => a - b);
-  for (let i = 0; i < arr.length; i++) {
-    let left = i + 1;
-    let right = arr.length - 1;
-    while (left < right) {
-      const sum = arr[i] + arr[left] + arr[right];
-      if (sum === 0) {
-        result.push([arr[i], arr[left], arr[right]]);
-        left++;
-        right--;
-      } else if (sum < 0) {
-        left++;
-      } else if (sum > 0) {
-        right--;
-      }
-    }
+function sum(n) {
+  let currentSum = n || 0;
+  function f(next) {
+    if (next === undefined) return currentSum;
+    currentSum += next;
+    return f;
   }
-  return result;
+  // f.toString = function () {
+  //   return currentSum;
+  // };
+  if (n == undefined) {
+    return 0;
+  } else {
+    return f;
+  }
 }
-const arr = [-1, 0, 1, 2, -1, -4];
-console.log(threeSum(arr));
+
+console.log(sum()); // 0
+console.log(sum(1)()); // 1
+console.log(sum(1)(2)(2)()); // 5
+const hh = sum(1)(3);
+console.log(hh()); // 4
