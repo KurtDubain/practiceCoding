a3ac086345f07f4c72d40dc87bcb100b1929a93f
Kurt.Dubain <kurt.du.cobain@gmail.com>
2024-03-15 15:16:23 +0800

    Update haha.js

--- haha.js
+++ haha.js
@@ -1,13 +1,14 @@
-const promise = new Promise((resolve, reject) => {
-  console.log(1);
-  setTimeout(() => {
-    console.log("timeStart");
-    resolve("success");
-    console.log("timeEnd");
-  }, 0);
-  console.log(2);
-});
-promise.then((res) => {
-  console.log(res);
-});
-console.log(4);
+// const promise = new Promise((resolve, reject) => {
+//   console.log(1);
+//   setTimeout(() => {
+//     console.log("timeStart");
+//     resolve("success");
+//     console.log("timeEnd");
+//   }, 0);
+//   console.log(2);
+// });
+// promise.then((res) => {
+//   console.log(res);
+// });
+// console.log(4);
+console.log(this);
