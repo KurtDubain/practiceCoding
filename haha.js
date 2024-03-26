1b1baed3d70fc2442f1fcb08732db97ccdad43de
Kurt.Dubain <kurt.du.cobain@gmail.com>
2024-03-26 10:48:20 +0800

    Update haha.js

--- haha.js
+++ haha.js
@@ -1,2 +1,21 @@
-console.log(1 && 2);
-ab;
+let x = 10;
+let obj = {
+  x: 20,
+  f: function () {
+    console.log(this.x);
+  },
+  s: function () {
+    console.log(this.x);
+    function fn() {
+      console.log(this.x);
+    }
+    return fn;
+  },
+};
+let fn = obj.f;
+fn();
+// 10
+obj.f();
+// 20
+obj.s()();
+// 10 20
