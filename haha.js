edc215a70e0c8339b6ba962e7ed40ffe3f6c04ca
Kurt.Dubain <kurt.du.cobain@gmail.com>
2024-04-10 16:31:14 +0800

    测试树的生成

--- haha.js
+++ haha.js
@@ -57,10 +57,204 @@
 // };
 // console.log(extractVariableNames(obj));
 
-function countLines(str) {
-  return str.split("\\n").length;
+// function countLines(str) {
+//   return str.split("\\n").length;
+// }
+
+// const pl0Code =
+//   "var i, sum;\\r\\nbegin\\r\\n    sum := 0;\\r\\n    for i := 1 to 5 do\\r\\n        begin\\r\\n            sum := sum + i;\\r\\n            write sum;\\r\\n        end;\\r\\n    end;\\r\\nend.";
+// console.log(countLines(pl0Code));
+function formatAST(node, depth = 0) {
+  let output = "";
+  const indent = "|  ".repeat(depth);
+  output += `${indent}${node.type}`;
+
+  if (node.name) {
+    output += `: ${node.name}`;
+  }
+
+  if (node.value) {
+    output += `: ${node.value}`;
+  }
+
+  output += "\n";
+
+  if (node.children) {
+    node.children.forEach((child) => {
+      output += formatAST(child, depth + 1);
+    });
+  }
+
+  if (node.statements) {
+    node.statements.forEach((statement) => {
+      output += formatAST(statement, depth + 1);
+    });
+  }
+
+  if (node.expression) {
+    output += `${indent}|— Expression\n`;
+    output += formatAST(node.expression, depth + 2);
+  }
+
+  if (node.condition) {
+    output += `${indent}|— Condition\n`;
+    output += formatAST(node.condition, depth + 2);
+  }
+
+  if (node.thenStatement) {
+    output += `${indent}|— ThenStatement\n`;
+    output += formatAST(node.thenStatement, depth + 2);
+  }
+
+  if (node.elseIfStatement) {
+    node.elseIfStatement.forEach((elseif, index) => {
+      output += `${indent}|— ElseIfStatement ${index + 1}\n`;
+      output += formatAST(elseif, depth + 2);
+    });
+  }
+
+  if (node.elseStatement) {
+    output += `${indent}|— ElseStatement\n`;
+    output += formatAST(node.elseStatement, depth + 2);
+  }
+
+  return output;
 }
 
-const pl0Code =
-  "var i, sum;\\r\\nbegin\\r\\n    sum := 0;\\r\\n    for i := 1 to 5 do\\r\\n        begin\\r\\n            sum := sum + i;\\r\\n            write sum;\\r\\n        end;\\r\\n    end;\\r\\nend.";
-console.log(countLines(pl0Code));
+// 使用示例
+const ast = {
+  type: "Program",
+  children: [
+    {
+      type: "Block",
+      children: [
+        {
+          type: "Declaration",
+          children: [{ type: "VarDeclaration", name: "x", line: 1 }],
+          line: 2,
+        },
+        {
+          type: "BeginEndBlock",
+          statements: [
+            {
+              type: "AssignmentStatement",
+              identifier: "x",
+              expression: { type: "Literal", value: "7", line: 3 },
+              line: 3,
+            },
+            {
+              type: "IfStatement",
+              condition: {
+                type: "BinaryExpression",
+                operator: ">",
+                left: { type: "Identifier", name: "x", line: 4 },
+                right: { type: "Literal", value: "10", line: 4 },
+                line: 4,
+              },
+              thenStatement: {
+                type: "BeginEndBlock",
+                statements: [
+                  {
+                    type: "AssignmentStatement",
+                    identifier: "x",
+                    expression: {
+                      type: "BinaryExpression",
+                      operator: "+",
+                      left: { type: "Identifier", name: "x", line: 6 },
+                      right: { type: "Literal", value: "1", line: 6 },
+                      line: 6,
+                    },
+                    line: 6,
+                  },
+                  {
+                    type: "WriteStatement",
+                    expression: { type: "Identifier", name: "x", line: 7 },
+                    line: 7,
+                  },
+                ],
+                line: 6,
+              },
+              elseIfStatement: [
+                {
+                  condition: {
+                    type: "BinaryExpression",
+                    operator: "=",
+                    left: { type: "Identifier", name: "x", line: 9 },
+                    right: { type: "Literal", value: "5", line: 9 },
+                    line: 9,
+                  },
+                  thenStatement: {
+                    type: "WriteStatement",
+                    expression: { type: "Identifier", name: "x", line: 10 },
+                    line: 10,
+                  },
+                },
+                {
+                  condition: {
+                    type: "BinaryExpression",
+                    operator: "<",
+                    left: { type: "Identifier", name: "x", line: 11 },
+                    right: { type: "Literal", value: "10", line: 11 },
+                    line: 11,
+                  },
+                  thenStatement: {
+                    type: "BeginEndBlock",
+                    statements: [
+                      {
+                        type: "AssignmentStatement",
+                        identifier: "x",
+                        expression: {
+                          type: "BinaryExpression",
+                          operator: "-",
+                          left: { type: "Identifier", name: "x", line: 13 },
+                          right: { type: "Literal", value: "1", line: 13 },
+                          line: 13,
+                        },
+                        line: 13,
+                      },
+                      {
+                        type: "WriteStatement",
+                        expression: { type: "Identifier", name: "x", line: 14 },
+                        line: 14,
+                      },
+                    ],
+                    line: 13,
+                  },
+                },
+              ],
+              elseStatement: {
+                type: "BeginEndBlock",
+                statements: [
+                  {
+                    type: "AssignmentStatement",
+                    identifier: "x",
+                    expression: {
+                      type: "BinaryExpression",
+                      operator: "*",
+                      left: { type: "Identifier", name: "x", line: 18 },
+                      right: { type: "Identifier", name: "x", line: 18 },
+                      line: 18,
+                    },
+                    line: 18,
+                  },
+                  {
+                    type: "WriteStatement",
+                    expression: { type: "Identifier", name: "x", line: 19 },
+                    line: 19,
+                  },
+                ],
+                line: 18,
+              },
+              line: 5,
+            },
+          ],
+          line: 3,
+        },
+      ],
+      line: 22,
+    },
+  ],
+  line: 22,
+};
+
+console.log(formatAST(ast));
